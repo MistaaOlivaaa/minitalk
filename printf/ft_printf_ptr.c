701a9d5759acbd7a87a2a04e9bbea6617c04b6b3
saadfahmitest <s.fahmi2618@uca.ac.ma>
2025-04-17 21:25:08 +0100

    minitalk

---
+++ printf/ft_printf_ptr.c
@@ -0,0 +1,56 @@
+#include "ft_printf.h"
+
+
+
+int len_ptr(uintptr_t nb)
+{
+        int len;
+        len = 0;
+
+        while(nb != 0)
+        {
+                len++;
+
+                nb = nb / 10;
+                
+        }
+
+        return(len);
+
+}
+
+void ft_put_ptr(uintptr_t nb)
+{
+        if(nb >= 16)
+        {
+                ft_put_ptr(nb / 10);
+                ft_put_ptr(nb % 10);
+
+        }
+        else
+        {
+                if(nb <= 9)
+                        ft_putchar(nb + '0');
+                else
+                        ft_putchar(nb - 10 + 'a');
+                
+        }
+}
+
+int ft_putptr(uintptr_t ptr)
+{
+        int char_printed;
+        char_printed = 0;
+
+        char_printed += write(1, "0x", 2);
+
+        if(ptr = 0)
+                char_printed += write(1, "0", 1);
+        else
+        {
+                    ft_put_ptr(ptr);
+                    char_printed += len_ptr(ptr);
+        }
+
+        return(char_printed);
+}
